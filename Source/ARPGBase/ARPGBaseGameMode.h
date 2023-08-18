3768cee810cc4ac268729d21443fbb68146a6c22
srkibria <skibria@9dimensionstudios.com>
2023-08-18 20:15:40 +0900

    Base Project
    Base C++ Project

---
+++ Source/ARPGBase/ARPGBaseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARPGBaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AARPGBaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AARPGBaseGameMode();
+};
+
+
+
